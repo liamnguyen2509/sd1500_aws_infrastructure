a86eb4fe9ffc33593db3496e0311e0a370511395
Nguyễn Thái Lâm <lamnt1553@hotmail.com>
2023-08-22 15:47:17 +0700

    remove data_user

--- ec2.tf
+++ ec2.tf
@@ -11,18 +11,4 @@ module "ec2_instance" {
   vpc_security_group_ids = [module.server_sg.security_group_id]
   subnet_id              = element(module.vpc.public_subnets, 0)
   tags = var.tags
-  
-  user_data = <<EOF
-    #!/bin/bash
-    sudo yum update -y
-    sudo yum install wget
-    sudo amazon-linux-extras install java-openjdk11
-    sudo amazon-linux-extras install epel -y
-    sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
-    sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-    sudo yum install jenkins -y
-    sudo systemctl enable jenkins
-    sudo systemctl start jenkins
-    sudo systemctl status jenkins
-  EOF
 }
--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "lamnguyent-vpc"
+  name = "assignment-vpc"
   cidr = "10.0.0.0/16"
 
   azs             = slice(data.aws_availability_zones.available.names, 0, 3)
